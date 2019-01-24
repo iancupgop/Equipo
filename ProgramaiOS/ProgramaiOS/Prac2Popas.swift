08870b45ab87f5fb26c48344eaeb26f55a13a407
VelazquezGerardo <velazquezgerardo998@gmail.com>
2019-01-24 08:33:52 -0600

    ArchivoGera

---
+++ ProgramaiOS/ProgramaiOS/Prac2Popas.swift
@@ -0,0 +1,9 @@
+//
+//  Prac2Popas.swift
+//  ProgramaiOS
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 1/24/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import Foundation
